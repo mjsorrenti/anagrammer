f295fcfe918f678b83987b34f5a5e58123706ebd
SORRENTI Michael <DXJ@dsone.3ds.com>
2017-12-12 16:02:34 -0500

    switch to a more systematic algorithm for anagram generation

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ var letters = document.getElementById('letterinput');
 var button = document.getElementById('run');
 button.addEventListener('click', createAnagrams);
 
-function createAnagrams() {
-    var original = letters.value;   //original text as entered
-    var anagrams = [];              //array to hold the list of anagrams
+function numAnagrams(original) {
+    //var original = letters.value;   //original text as entered
+    //var anagrams = [];              //array to hold the list of anagrams
     var numAnagrams = 1;            //total number of possible anagrams (calculated below)
-    var buildingArray = true;       //flag to determine when all possible anagrams have been found
+    //var buildingArray = true;       //flag to determine when all possible anagrams have been found
     
     //determine the number of possible anagrams
     //factorial of the number of letters gives the total number of permutations
@@ -37,43 +37,67 @@ function createAnagrams() {
         numAnagrams = numAnagrams / occurenceDivisor;
     }
              
-    
-     
-    //loop to build the array of possible anagrams
-    while (buildingArray) {
+    return numAnagrams;
+}
+
+function createAnagrams() {
+    var letterArray = letters.value.split('');  //original text as entered
+    var selectorArray = []; //this array carries a sequence of index values used to extract letters from the original string
+    var keepAnagramming = true; //this is a flag that tells the anagram builder when it's done
+    var anagram = ''; //this variable holds temporary anagrams
+    var anagramCollection = []; //this array holds all the valid anagrams
+    var dupe = false; //flag to note that a new anagram has already been found
         
-        //get a new anagram
-        var testAnagram = randomWord(original);
- 
-        //if the new anagram already appears in the array, remove the original instance before inserting the new instance
-        for (j=0; j < anagrams.length; j++) {
-            if (anagrams[j] === testAnagram) {anagrams.splice(j, 1);}
-        }
     
-        //insert the new anagram in the array
-        anagrams.push(testAnagram);
-                
-        //check to see if all possible anagrams have been calculated
-        if (anagrams.length >= numAnagrams) {buildingArray = false;}
+    //Build the initial selector array
+    for (var i=0; i<letterArray.length; i++) {
+        selectorArray.push(0);
     }
     
-
-    writeResults(anagrams);
-}
-
-function randomWord(input) {
-    //construct a new anagram by randomly picking apart the letters in the input string and reassembling them in a new string
-    var tempWord = input;
-    var newWord = '';
-    for (i = 0; i < input.length; i++) {
-        var index = Math.floor(Math.random()*tempWord.length);
-        var extractedLetter = tempWord[index];
-        newWord = newWord + extractedLetter;
-        tempWord = tempWord.slice(0,index) + tempWord.slice(index+1);
+    //This is the main loop for building anagrams
+    while (keepAnagramming) {
+        
+        //This loop builds a string by iterating through the selection indexes in the current selectorArray
+        for (var position=0; position<selectorArray.length; position++) {
+            anagram += letterArray.splice(selectorArray[position],1);
         }
-    return newWord;
+        
+        //if the new anagram already appears in the array, set flag to not add it
+        for (j=0; j < anagramCollection.length; j++) {
+            if (anagramCollection[j] === anagram) {
+                dupe = true;
+                break;
+            }
+        }
+        
+        //Store the new anagram and reset the temp variables
+        if (!dupe) {anagramCollection.push(anagram);}
+        anagram = '';
+        dupe = false;
+        letterArray = letters.value.split('');
+        
+        
+        //This loop updates the selectorArray by checking current values from the end to the
+        //beginning. If a value can be incremented, it is and the loop breaks; If a value is
+        //at its max, it is reset to 0 and the loop continues to the next value. When the first
+        //item in the array is about to increment past its max, the main anagrammer loop breaks. 
+        for (position=selectorArray.length-2; position>=0; position--) {
+            if (selectorArray[position] < (selectorArray.length - position - 1)) {
+                selectorArray[position] = selectorArray[position] + 1;
+                break;
+                }
+            else if (position == 0) {
+                keepAnagramming = false;
+            }
+            else {
+                    selectorArray[position] = 0;
+            }
+        }
+    }
+    
+    writeResults(anagramCollection);
 }
-
+     
 function writeResults(wordArray) {
     var resultsDiv = document.querySelector('#anagrams');
     var newPara;
